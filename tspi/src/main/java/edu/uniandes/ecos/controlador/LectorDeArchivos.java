394af740b3aaecc853e9ba326f0b25d16c3403fc
Alvaro Suarez <asan123456@gmail.com>
2015-04-04 13:38:39 -0500

    incluyendo ejecricio 1: alvaro

--- tspi/src/main/java/edu/uniandes/ecos/controlador/LectorDeArchivos.java
+++ tspi/src/main/java/edu/uniandes/ecos/controlador/LectorDeArchivos.java
@@ -5,10 +5,71 @@
  */
 package edu.uniandes.ecos.controlador;
 
+import edu.uniandes.ecos.modelo.Data.DataEjercicio1_DatosClase;
+import edu.uniandes.ecos.modelo.Data.DataEjercicio1_DatosProyecto;
+import edu.uniandes.ecos.modelo.Data.Logica.LogicaEjercicio1_Contador;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 /**
  *
  * @author JAVIER
  */
-public class LectorDeArchivos {
+public class LectorDeArchivos extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
+        mostrarLineas(req, resp);
+    }
     
+    private void mostrarLineas(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
+        LogicaEjercicio1_Contador contador = new LogicaEjercicio1_Contador();
+        ArrayList<DataEjercicio1_DatosProyecto> listaProyectos = contador.conteoArchivos("projects");
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<table border='1'>");
+        out.println("<tr>");
+        out.println("<th>Nombre Proyecto</th>");
+        out.println("<th>Nombre Clase</th>");
+        out.println("<th>Numero Lineas</th>");
+        out.println("<th>Numero Metodos</th>");
+        out.println("</tr>");
+        for(DataEjercicio1_DatosProyecto proyecto: listaProyectos){
+            int i = 0;
+            out.println("<tr align='center'>");
+            out.println("<td>"+proyecto.getNombreProyecto()+"</td>");
+            for(DataEjercicio1_DatosClase datosClase: proyecto.getDatosClase()){
+                if(i>0){
+                    out.println("<tr align='center'>");
+                    out.println("<td></td>");
+                }
+                out.println("<td>"+datosClase.getNombreClase()+"</td>");
+                out.println("<td>"+datosClase.getNumeroLineas()+"</td>");
+                out.println("<td>"+datosClase.getNumeroMetodos()+"</td>");
+                out.println("</tr>");
+                i++;
+            }
+        }
+        out.println("</table>");
+        out.println("</body>");
+        out.println("</html>");
+        resp.getWriter().print(out);
+    }
+    
+    public static void main(String[] args) throws Exception{
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new LectorDeArchivos()),"/*");
+        server.start();
+        server.join();
+    }     
 }
